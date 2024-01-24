eee23cb035fc0818e125f00d421b1d17e1f22994
techpilot <ngwustephen99@gmail.com>
2024-01-24 19:38:30 +0100

    updates on server code

--- teleLegalSite/expressRoutes.js
+++ teleLegalSite/expressRoutes.js
@@ -1,22 +1,61 @@
 const app = require("./server").app;
 const jwt = require("jsonwebtoken");
 const linkSecret = "thisisasecret";
+const { v4: uuidv4 } = require("uuid");
+
+// normally this comes from the db
+const professionalAppointments = [
+  {
+    professionalsFullName: "Peter Chan, J.D",
+    apptDate: Date.now() + 500000,
+    uuid: 1,
+    clientName: "Jim Jones",
+  },
+  {
+    professionalsFullName: "Peter Chan, J.D",
+    apptDate: Date.now() - 2000000,
+    uuid: 2, // uuid:uuidv4(),
+    clientName: "Akash Patel",
+  },
+  {
+    professionalsFullName: "Peter Chan, J.D",
+    apptDate: Date.now() + 10000000,
+    uuid: 3, //uuid:uuidv4(),
+    clientName: "Mike Williams",
+  },
+];
+
+app.set("professionalAppointments", professionalAppointments);
 
 app.get("/user-link", (req, res) => {
-  const apptData = {
-    professionalFullName: "Robert Bunch, J. D",
-    apptDate: Date.now(),
-  };
+  const uuid = uuidv4(); // standing in for uniques id or primary key in the database
+
+  const apptData = professionalAppointments[0]
+
+  // professionalAppointments.push(apptData);
 
   // encode the data in a token
   const token = jwt.sign(apptData, linkSecret);
-  res.send(`https://localhost:5173/join-video?token=${token}`)
+  res.send(`https://localhost:5173/dashboard/inbox?token=${token}`);
+  // console.log(token);
 
   res.json("This is a test route");
 });
 
 app.post("/validate-link", (req, res) => {
-    const token = req.body.token
-    const decodedData = jwt.verify(token, linkSecret)
-    res.json(decodedData)
-})
+  const token = req.body.token;
+  const decodedData = jwt.verify(token, linkSecret);
+  // console.log(professionalAppointments)
+  res.json(decodedData);
+});
+
+app.get("/pro-link", (req, res) => {
+  const userData = {
+    fullName: "Peter Chan, J.D",
+    proId: 1234,
+  };
+  const token = jwt.sign(userData, linkSecret);
+  res.send(
+    `<a href="https://localhost:5173/dashboard/join-video?token=${token}" target="_blank">Link here</a>`
+  );
+});
--- teleLegalSite/socketServer.js
+++ teleLegalSite/socketServer.js
@@ -1,5 +1,111 @@
-const io = require("./server").io
+const io = require("./server").io;
+const app = require("./server").app;
+const jwt = require("jsonwebtoken");
+const linkSecret = "thisisasecret";
 
-io.on('connection', socket => {
-    console.log(socket.id, "has connected")
-})
+const connectedProfessionals = [];
+
+const allKnownOffers = {
+  // uniqueId - key
+  //offer
+  //professionalsFullName
+  //clientName
+  //apptDate
+  //offererIceCandidates
+  //answer
+  //answerIceCandidates
+};
+
+io.on("connection", (socket) => {
+  console.log(socket.id, "has connected");
+
+  const handshakeData = socket.handshake.auth.jwt;
+
+  let decodedData;
+  try {
+    decodedData = jwt.verify(handshakeData, linkSecret);
+  } catch (err) {
+    console.log(err);
+    socket.disconnect();
+
+    return;
+  }
+
+  const { fullName, proId } = decodedData;
+
+  if (proId) {
+    const connectedPro = connectedProfessionals.find(
+      (cp) => cp.proId === proId
+    );
+
+    if (connectedPro) {
+      connectedPro.socketId = socket.id;
+    } else {
+      connectedProfessionals.push({
+        socketId: socket.id,
+        fullName,
+        proId,
+      });
+    }
+
+    const professionalAppointments = app.get("professionalAppointments");
+
+    // professionalAppointments.push(apptData);
+    socket.emit(
+      "apptData",
+      professionalAppointments.filter(
+        (pa) => pa.professionalsFullName === fullName
+      )
+    );
+
+    for (key in allKnownOffers) {
+      if (allKnownOffers[key].professionalsFullName === fullName) {
+        io.to(socket.id).emit("newOfferWaiting", allKnownOffers[key]);
+      }
+    }
+  } else {
+    //
+  }
+
+  //   console.log(connectedProfessionals);
+
+  socket.on("newOffer", ({ offer, apptInfo }) => {
+    // console.log(offer);
+    // console.log("==============");
+    // console.log(apptInfo);
+
+    allKnownOffers[apptInfo.uuid] = {
+      ...apptInfo,
+      offer,
+      offererIceCandidates: [],
+      answer: null,
+      answerIceCandidates: [],
+    };
+
+    const professionalAppointments = app.get("professionalAppointments");
+    const pa = professionalAppointments.find((pa) => pa.uuid === apptInfo.uuid);
+    console.log("prof", apptInfo);
+
+    if (pa) {
+      pa.waiting = true;
+    }
+
+    const p = connectedProfessionals.find(
+      (cp) => cp.fullName === apptInfo.professionalsFullName
+    );
+    if (p) {
+      // only emit if professional is connected
+      const socketId = p.socketId;
+      socket
+        .to(socketId)
+        .emit("newOfferWaiting", allKnownOffers[apptInfo.uuid]);
+
+      socket.to(socketId).emit(
+        "apptData",
+        professionalAppointments.filter(
+          (pa) => pa.professionalsFullName === apptInfo.professionalsFullName
+        )
+      );
+    }
+  });
+});
